df6a1a603f81da7f5ddb82411c0ff0221cc45014
NICOMB22 <nicolasmaldonadob@gmail.com>
2023-04-11 14:56:02 -0500

    algoritmo envejecimiento

--- src/MemoriaVirtual.java
+++ src/MemoriaVirtual.java
@@ -82,5 +82,86 @@ public class MemoriaVirtual {
 		return MAP;
 	
 	}
+
+    public void algoritmoEnvejecimiento(int[][] MAP, int[][] MemoriaVirtual){
+        int filasMAP = MAP.length;
+        int filasMemoriaVirtual = MemoriaVirtual.length;
+        int numFallos = 0;
+        int menor = 10000;
+        int llave = 0;
+        boolean centinela = false;
+
+        for(int i=0; i<filasMAP; i++){
+            if(MAP[i][1] == -1){
+                centinela = false;
+                numFallos++;
+                //Iterar sobre la RAM
+                for(int j=0; j<filasMemoriaVirtual; j++){
+                    //Caso en el que hay espacios en la Memoria Virtual que no han sido modificados aun
+                    if(MemoriaVirtual[j][1] == 0000){
+                        centinela = true;
+                        MAP[i][1] = i;
+                        MemoriaVirtual[j][1] = 1000;
+                        //Si se esta cambiando el valor en un espacio de la RAM que no sea el 0, se actualizan los anteriores
+                        if (j>0){
+                            for(int k=0; k<j; k++){
+                                if(MemoriaVirtual[k][1] == 1000){
+                                    MemoriaVirtual[k][1] = 0100;
+                                }
+                                else if(MemoriaVirtual[k][1] == 0100){
+                                    MemoriaVirtual[k][1] = 0010;
+                                }
+                                else if(MemoriaVirtual[k][1] == 0010){
+                                    MemoriaVirtual[k][1] = 0001;
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    //En caso de que no haya ningun espacio en la RAM con valor 0000, se encuentra el menor:
+                    else{
+                    //Convertir el valor del binario de la Memoria Virtual a decimal
+                    String binario = Integer.toString(MemoriaVirtual[j][1]);
+                    int valor = convertirBinarioADecimal(binario);
+                    if (valor < menor){
+                        menor = valor;
+                        llave = j;
+                    }
+                    }
+ 
+                }
+                if (centinela == false){
+                    MAP[llave][1] = -1;
+                    MAP[i][1] = llave;
+
+                    if(MemoriaVirtual[llave][1] == 0001){
+                        MemoriaVirtual[llave][1] = 1001;
+                    }
+                    else if(MemoriaVirtual[llave][1] == 0010){
+                        MemoriaVirtual[llave][1] = 1010;
+                    }
+                    else if(MemoriaVirtual[llave][1] == 0100){
+                        MemoriaVirtual[llave][1] = 1100;
+                    }    
+                }
+            }
+        }
+    }
+
+    public static int convertirBinarioADecimal(String binario){
+        int decimal = 0;
+        int longitud = binario.length();
+
+        for (int i = 0; i<longitud; i++){
+            char digitoBinario = binario.charAt(i);
+            if(digitoBinario == '1'){
+                decimal += Math.pow(2, longitud-i-1);
+            }
+            else if(digitoBinario != '0'){
+                throw new IllegalArgumentException("El numero binario debe contener solo 0s y 1s");
+            }
+        }
+        return decimal;
+    }
     
 }
